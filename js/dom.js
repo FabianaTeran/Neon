8a17d2d6f922e4a505f7c17c00886d36eaff230f
Fabiana Teran <fabyvayne@gmail.com>
2022-01-27 19:07:22 -0300

    cambios1

---
+++ js/dom.js
@@ -0,0 +1,42 @@
+    class TodosLosProductos {
+        constructor(nombre, precio, marca, categoria, imagen) {
+        this.nombre = nombre.toUpperCase();
+        this.precio = parseFloat(precio);
+        this.marca = marca.toUpperCase();
+        this.categoria = categoria;
+        this.imagen = imagen;
+        }
+    }
+    
+    const producto = [];
+
+producto.push(new TodosLosProductos("Nike Pegasus Trail 3", "23.999", "Nike", "women","../imagenes/1.jpeg"));
+producto.push(new TodosLosProductos("Nike Air Max Excee S50", "18.499", "nike", "Men","../imagenes/2.jpeg"));
+producto.push( new TodosLosProductos("Nike Revolution 6 Next Nature", "12.499", "nike", "women","../imagenes/3.jpeg"));
+producto.push(new TodosLosProductos("Nike Downshifter 11", "13.499", "nike", "men","../imagenes/4.jpeg"));
+producto.push(new TodosLosProductos(" Nike Quest 4", "15.499", "nike", "men","../imagenes/6.jpeg"));
+
+
+    const contenedor = document.getElementById("cardProductos");
+    producto.forEach((productos) => {
+    let card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <div>
+        <img  class="card__img" src="${productos.imagen}">
+        <div class="card__content">
+        <h5 class="card__title">${productos.nombre}</h5>
+        <p class="card__text">${productos.precio}</p>
+        <a href="#cart" class="btn" id= "comprar" >  Comprar </a>
+    </div>
+
+    `;
+    contenedor.appendChild(card);
+    });
+
+    const carrito = document.getElementById("comprar");
+carrito.addEventListener("click", llenarCarrito);
+
+function llenarCarrito() {
+    console.log("Agregaste un producto a tu carrito");
+}
--- js/scrips.js
+++ js/scrips.js
@@ -1,94 +1,71 @@
-// mi pagina web será una tienda de zapatillas, el usuario tendra que agregar los productos al carrito de compra, en ahi se hará una suma de todos los productos 
-// cuenta con la opcion de filtrar, en este paso puede elegir entre los productos de mayor y menos costo. tambien por la marca.
-class TodosLosProductos {
-    constructor(nombre, precio, marca, categoria) {
-    this.nombre = nombre.toUpperCase();
-    this.precio = parseFloat(precio);
-    this.marca = marca.toUpperCase();
-    this.categoria = categoria;
-    }
-}
 
-const producto = [];
+// const menorCosto = producto.filter((producto) => producto.precio <= 1500);
+// console.log(menorCosto);
 
-producto.push(new TodosLosProductos(" st329 cmf", "1500", "k-swiss", "women"));
-producto.push(new TodosLosProductos("Air nike force", "2131", "nike", "Men"));
-producto.push( new TodosLosProductos("Air Zoom Pegasus ", "3222", "nike", "women"));
-producto.push(new TodosLosProductos("Legend 9 Elite Fg", "1300", "nike", "men"));
-producto.push(new TodosLosProductos("FORTARUN LACE RUNNING", "1700", "adidas", "men"));
-producto.push(new TodosLosProductos(" U_PATH X", "2000", "adidas", "women"));
-producto.push(new TodosLosProductos(" SL ANDRIDGE", "2012", "adidas", "women"));
-producto.push(new TodosLosProductos(" COURT DRIVE VANTAGE", "2500", " LACOSTE", "women"));
+// const mayorCosto = producto.filter((producto) => producto.precio > 1500);
+// console.log(mayorCosto);
 
-console.log(producto);
+// const marcaKwiss = producto.find(producto => producto.marca == "K-SWISS");
+// console.log(marcaKwiss);
 
-const menorCosto = producto.filter((producto) => producto.precio <= 1500);
-console.log(menorCosto);
-
-const mayorCosto = producto.filter((producto) => producto.precio > 1500);
-console.log(mayorCosto);
-
-const marcaKwiss = producto.find(producto => producto.marca == "K-SWISS");
-console.log(marcaKwiss);
-
-const marcaAdidas = producto.find(producto => producto.marca == "ADIDAS");
-console.log(marcaAdidas);
+// const marcaAdidas = producto.find(producto => producto.marca == "ADIDAS");
+// console.log(marcaAdidas);
 
 
 
 
-//  despues tendrá la opcion de hacer pagos por cuotas. 
-// tambien tendrá la opcion  de escribir el nombre de los productos y seleccionarlo.
+// //  despues tendrá la opcion de hacer pagos por cuotas. 
+// // tambien tendrá la opcion  de escribir el nombre de los productos y seleccionarlo.
 
 
-let precio = parseInt(prompt("Precio total: "))
-let cantidadCuotas = parseInt(prompt("Cuantas cuotas: "));
-function cuotas(precio, cantidadCuotas){
-    if(cantidadCuotas == 2){
-        alert("El monto total de su compra es: " + precio / cantidadCuotas);
-    }
-    else if(cantidadCuotas == 6){
-        alert("El monto total de su compra es: " + (precio / cantidadCuotas) + ((precio * 2 ) / 100 ));
-    }
-    else if(cantidadCuotas == 10){
-        alert("El monto total de su compra es: " + (precio / cantidadCuotas) + ((precio * 5 ) / 100 ));
-    }
-    else{
-        alert("El monto total de su compra es: " + (precio / cantidadCuotas) + ((precio * 7 ) / 100 ));
-    }
-    }
-    cuotas(precio , cantidadCuotas);
+// let precio = parseInt(prompt("Precio total: "))
+// let cantidadCuotas = parseInt(prompt("Cuantas cuotas: "));
+// function cuotas(precio, cantidadCuotas){
+//     if(cantidadCuotas == 2){
+//         alert("El monto total de su compra es: " + precio / cantidadCuotas);
+//     }
+//     else if(cantidadCuotas == 6){
+//         alert("El monto total de su compra es: " + (precio / cantidadCuotas) + ((precio * 2 ) / 100 ));
+//     }
+//     else if(cantidadCuotas == 10){
+//         alert("El monto total de su compra es: " + (precio / cantidadCuotas) + ((precio * 5 ) / 100 ));
+//     }
+//     else{
+//         alert("El monto total de su compra es: " + (precio / cantidadCuotas) + ((precio * 7 ) / 100 ));
+//     }
+//     }
+//     cuotas(precio , cantidadCuotas);
 
 
- //una vez que el cliente eligio la forma de pago, se piden los datos(que en este paso ya los tengo tipeados para usar el constructor de Cliente) para poder enviarle las indicaciones y estado  de su pedido por mail o llamada   
-    class Cliente {
-        constructor(entrada) {
-        this.nombre = entrada.nombre;
-        this.apellido = entrada.apellido;
-        this.email = entrada.email;
-        this.telefono = entrada.telefono;
-        this.productosComprados = entrada.productosComprados;
-        }
-        hablar() {
-        console.log(
-            "Gracias por elegirnos, Corroborá las instrucciones en el correo: " +
-            this.email
-        );
-        }
-    }
+//  //una vez que el cliente eligio la forma de pago, se piden los datos(que en este paso ya los tengo tipeados para usar el constructor de Cliente) para poder enviarle las indicaciones y estado  de su pedido por mail o llamada   
+//     class Cliente {
+//         constructor(entrada) {
+//         this.nombre = entrada.nombre;
+//         this.apellido = entrada.apellido;
+//         this.email = entrada.email;
+//         this.telefono = entrada.telefono;
+//         this.productosComprados = entrada.productosComprados;
+//         }
+//         hablar() {
+//         console.log(
+//             "Gracias por elegirnos, Corroborá las instrucciones en el correo: " +
+//             this.email
+//         );
+//         }
+//     }
     
-    const cliente1 = new Cliente({
-        nombre: "Fabiana",
-        apellido: "Vayne",
-        email: "fabyvayne@gmail.com",
-        telefono: "1170344202",
-        productosComprados: 3
-    });
+//     const cliente1 = new Cliente({
+//         nombre: "Fabiana",
+//         apellido: "Vayne",
+//         email: "fabyvayne@gmail.com",
+//         telefono: "1170344202",
+//         productosComprados: 3
+//     });
     
-    const { nombre, apellido, email, telefono, productosComprados } = cliente1;
+//     const { nombre, apellido, email, telefono, productosComprados } = cliente1;
     
-    console.log(nombre, apellido, email, telefono, productosComprados);
-    cliente1.hablar();
+//     console.log(nombre, apellido, email, telefono, productosComprados);
+//     cliente1.hablar();
     
 
 
